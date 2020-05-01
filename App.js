5a722317176b0c2095ddb84b5182e1fb9ed5c94d
Raphael Carlos Rego <raphaelcarlosr@gmail.com>
2020-05-01 15:43:19 -0300

    Set hello world

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+   constructor(props){
+     super(props);
+     this.state = {
+     name: 'World!',
+     }
+   }
+   onClick = () => {
+     this.setState({
+     name: 'John!',
+     })
+   };
+   render() {
+   return (
+     <View style={styles.container}>
+       <Text>Hello {this.state.name}</Text>
+       <Button 
+            onPress={() => {this.onClick()}} 
+            title='Click me'
+            color='#4169E1'>
+       </Button>
+     </View>
+   );
+   }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+     flex: 1,
+     backgroundColor: '#fff',
+     alignItems: 'center',
+     justifyContent: 'center',
+   }, 
+   nameText: {
+     fontSize: 50,
+     padding: 15,
+   }
 });
+
